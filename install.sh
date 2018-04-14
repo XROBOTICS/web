e918da7ffc0ee67d99b86da3593f0999958836ba
Le0nX <cloudmindone@gmail.com>
2018-04-14 14:17:06 +0300

    fix: install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get install python3
 sudo apt-get install python3-django
 sudo apt-get install pip3
 sudo pip3 install virtualenv virtualenvwrapper
-echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3">> .bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh">> .bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3">> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh">> ~/.bashrc
 
 
